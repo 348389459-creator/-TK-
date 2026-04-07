f451bd1c4337aac70620e7a260d9ce27c39d2e49
348389459-creator <348389459@qq.com>
2026-04-07 08:03:00 +0000

    增加使用教程

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -16,7 +16,7 @@ export default function Tutorial() {
             autoPlay 
             className="w-full aspect-video outline-none"
           >
-            <source src="https://tianqitk-1379115483.cos.ap-guangzhou.myqcloud.com/1.mp4" type="video/mp4" />
+            <source src="https://tianqitk-1379115483.cos.ap-guangzhou.myqcloud.com/tutorial.mp4" type="video/mp4" />
             您的浏览器不支持视频播放，请更换谷歌浏览器。
           </video>
         </div>
